90ec858107c72386f98b56862a758962b8e685a5
Esteban Lescano <esteban.lescano@bitlogic.io>
2022-10-13 09:15:35 -0300

    inicio

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +14,17 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute ("titulo","hola spring Fremework con Model!!");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil (Model model) {
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Esteban");
+        usuario.setApellido("Lescano");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute ("titulo","Perfil del usuario: " .concat(usuario.getNombre()));
+        return "perfil";
     }
 }
